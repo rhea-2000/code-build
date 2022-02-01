01f73ca81e18f545d3c89071563a9b17a2d0416d
rhea-2000 <97457769+rhea-2000@users.noreply.github.com>
2022-02-01 19:10:01 +0530

    Delete script directory

--- script/dockerBuild.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-DOCKER_REPO=`aws ecr describe-repositories --repository-names <created_repo_name> | grep repositoryUri | cut -d "\"" -f 4`
-Docker build --no-cache -t ${DOCKER_REPO}:<docker_tag> .
-Docker push ${DOCKER_REPO}:<docker_tag>
